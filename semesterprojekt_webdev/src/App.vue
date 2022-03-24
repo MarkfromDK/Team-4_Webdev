41bf398704cbc2e4968f01f016ba0be356ee2c17
MarkfromDK <mark4023@edu.ucl.dk>
2022-03-24 18:54:14 +0100

    Header lavet og filer tilføjet

--- semesterprojekt_webdev/src/App.vue
+++ semesterprojekt_webdev/src/App.vue
@@ -1,14 +1,17 @@
 <template>
     <SiteNavbar />
+    <SiteHeader />
 </template>
 
 <script>
 import SiteNavbar from "./components/SiteNavbar.vue";
+import SiteHeader from "./components/SiteHeader.vue";
 
 export default {
     name: "App",
     components: {
         SiteNavbar,
+        SiteHeader,
     },
 };
 </script>
