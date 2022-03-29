4fd0d513385228171e79aeafe3a1e8be4d9d7dc7
Laurits92 <34508820+Laurits92@users.noreply.github.com>
2022-03-29 08:46:27 +0200

    footer hhelp

--- semesterprojekt_webdev/src/components/SiteFooter.vue
+++ semesterprojekt_webdev/src/components/SiteFooter.vue
@@ -1,5 +1,47 @@
 <template>
     <div class="SiteFooter"> 
-        <p>something footer </p>
+        <div class="footer">
+        <a class= "footer__logo" href="#">
+            <img src="/images/logo.svg" />
+        </a>
+        <div class="footer__department">
+            <h2>Afdelinger</h2>
+            <ul class="department__list">
+            <li><a href="#">Playstation 5</a></li>
+            <li><a href="#">Playstation 4</a></li>
+            <li><a href="#">Nintendo Switch</a></li>
+            <li><a href="#">Xbox Series X</a></li>
+            <li><a href="#">Xbox One</a></li>
+            <li><a href="#">Xbox 360</a></li>
+            <li><a href="#">PC</a></li>
+            <li><a href="#">PC Download</a></li>
+            <li><a href="#">Nintendo 3DS</a></li>   
+            </ul>
+        </div>
+
+        <div class="footer__contact">
+             <h2>Kontakt</h2>
+            <a href="#">Seebladsgade 1, 5000 Odense</a>
+            <p>+45 12 34 56 78</p>
+        </div>
+
+        <div class="footer__social">
+            <h2>Social</h2>
+            <ul>
+           <li><a href="#">
+                <img src="/images/facebook.svg" alt="Facebook">
+            </a></li>
+                <li><a href="#">
+                <img src="/images/instagram.svg" alt="Instagram">
+            </a></li>
+                <li><a href="#">
+                <img src="/images/twitter.svg" alt="Twitter">
+            </a></li>
+            </ul>
+        </div>
+        </div>
+       <div class="copyrightText">
+           <p>laurits is supreme leader of the site</p>
+       </div>
     </div>
 </template>
