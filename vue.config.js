d1a3245b281ac6a5ca9d07c1c913499646da7f9c
MarkfromDK <60467792+MarkfromDK@users.noreply.github.com>
2022-03-29 20:13:23 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+    publicPath: '/Team-4_Webdev/semesterprojekt_webdev/'
+}
