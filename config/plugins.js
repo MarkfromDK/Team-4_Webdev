bb2a1a1344c27ae9e5d223022e47d8249a255ab5
Benjamin Porsgaard <benjamin_porsgaard@hotmail.com>
2022-05-23 13:29:23 +0200

    opdateret backend

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,16 @@
+// path: ./config/plugins.js
+
 module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AKIAYZM5YQAPXFEN3RMP"),
-        secretAccessKey: env("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"),
-        region: env("eu-north-1"),
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
         params: {
-          Bucket: env("t4-projekt-bucket"),
+          Bucket: env("AWS_BUCKET"),
         },
       },
     },
